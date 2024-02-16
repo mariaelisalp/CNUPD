0f8aef9ef6319a454c5631cd57be5f3be53c70cd
Bernardo Gomes Nocera <57022140015@unicentro.edu.br>
2024-02-15 23:16:39 -0300

    Mudanças no login
    Nessa última commit só foi feito tradução de alguns textos, tentei configurar o "Esqueci minha senha" para testar, mas não consegui. @lsouzapedroso

--- CNUPD/resources/views/auth/confirm-password.blade.php
+++ CNUPD/resources/views/auth/confirm-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+        {{ __('Essa é uma área segura do aplicativo. Confirme sua senha antes de prosseguir.') }}
     </div>
 
     <form method="POST" action="{{ route('password.confirm') }}">
